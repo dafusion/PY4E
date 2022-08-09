996a367b6e80ee43abb5e99b3ad2b34b7bd36744
data fusion <109286852+dafusion@users.noreply.github.com>
2022-08-09 19:48:59 +0900

    Update #2.py

--- 4주차/#2.py
+++ 4주차/#2.py
@@ -2,3 +2,40 @@
 #글은 어떤 글이 좋습니다. 인터넷에서 검색해서 복사 붙여넣기로 변수에 넣어 줍니다.
 #변수에 담긴 글을 함수에 넣어주면 txt 파일로 저장도 함께 되도록 해줍니다.
 
+def count_word(text, word):
+
+    text_save = open("text.txt", "w", encoding="UTF8")
+
+    text_save.write(text)
+
+    text_save.close()
+
+    
+
+    count = 0 
+
+    word_length = len(word) 
+
+    word_save = "" 
+
+    
+
+    f_1 = open("text.txt") 
+
+    for line in f_1: 
+
+        if word in line:
+
+            for s in line:
+
+                word_save = word_save + s 
+
+                if word_save == word: 
+
+                    count += 1 # count +1
+
+                if len(word_save) == len(word): 
+
+                    word_save = word_save[1:]
+
+    print(count) 
